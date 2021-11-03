1a3d03b64141e380cf4efee19a859815859f6fd2
horvathsanyi <horvathsanyi0202@gmail.com>
2021-11-03 13:56:10 +0100

    Filled uti.py

--- model/util.py
+++ model/util.py
@@ -7,4 +7,9 @@ def generate_id(number_of_small_letters=4,
                 number_of_digits=2,
                 number_of_special_chars=2,
                 allowed_special_chars=r"_+-!"):
-    return 'T!uq6-b4Yq'
+    generated_id = [random.choice(string.ascii_lowercase) for char in range(number_of_small_letters)]
+    generated_id += [random.choice(string.ascii_uppercase) for char in range(number_of_capital_letters)]
+    generated_id += [random.choice(string.digits) for char in range(number_of_digits)]
+    generated_id += [random.choice(allowed_special_chars) for char in range(number_of_special_chars)]
+    random.shuffle(generated_id)
+    return "".join(generated_id)
