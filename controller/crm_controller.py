41ca4c9af37316f1ad1e8019b08bb3e639e1dfc6
mrdzsi <jenofigergo@gmail.com>
2021-11-04 20:23:40 +0100

    Cleaned typos

--- controller/crm_controller.py
+++ controller/crm_controller.py
@@ -8,16 +8,16 @@ def list_customers():
 
 
 def add_customer():
-    name = view.get_input("Please enter your name")
-    email = view.get_input("Please enter your email adress")
+    name = view.get_input("Please enter your name: ")
+    email = view.get_input("Please enter your email adress: ")
     subscription = view.get_input("Are you subscibed? 1 : yes, 0 :  no")
     crm.new_customer(name, email, subscription)
 
 
 def update_customer():
-    id = view.get_input("Please enter your id")
-    name = view.get_input("Please enter your new name")
-    email = view.get_input("Please enter your new email adress")
+    id = view.get_input("Please enter your id: ")
+    name = view.get_input("Please enter your new name: ")
+    email = view.get_input("Please enter your new email adress: ")
     subscription = view.get_input("Are you subscibed? 1 : yes, 0 :  no")
     is_updated = crm.change_customer_data(id, name, email, subscription)
     if is_updated is False:
@@ -25,8 +25,8 @@ def update_customer():
 
 
 def delete_customer():
-    id = view.get_input("Please enter the id for deletion")
-    is_deleted = crm.change_customer_data(id)
+    id = view.get_input("Please enter the id for deletion: ")
+    is_deleted = crm.remove_customer_data(id)
     if is_deleted is True:
         view.print_error_message("ID not found")
 
@@ -67,7 +67,7 @@ def menu():
     while operation != '0':
         display_menu()
         try:
-            operation = view.get_input("Select an operation")
+            operation = view.get_input("Select an operation: ")
             run_operation(int(operation))
         except KeyError as err:
             view.print_error_message(err)
