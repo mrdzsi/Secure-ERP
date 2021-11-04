ba316e7713439b1a68d20d151ccfc6c3e4626461
mrdzsi <jenofigergo@gmail.com>
2021-11-04 20:09:09 +0100

    Changed parameter in functions

--- model/crm/crm.py
+++ model/crm/crm.py
@@ -11,51 +11,51 @@ Data table structure:
 from model import data_manager, util
 
 
+DATAFILE = "model/crm/crm.csv"
+HEADERS = ["id", "name", "email", "subscribed"]
+
+
 def new_customer(name, email, subscription):
     id = util.generate_id()
     data = [id, name, email, subscription]
-    customers = data_manager.read_table_from_file('crm.csv', ';')
+    customers = data_manager.read_table_from_file(DATAFILE, ';')
     customers.append(data)
-    data_manager.write_table_to_file('crm.csv', customers, ';')
+    data_manager.write_table_to_file(DATAFILE, customers, ';')
 
 
 def list_of_customers():
-    customers = data_manager.read_table_from_file('crm.csv', ';')
+    customers = data_manager.read_table_from_file(DATAFILE, ';')
     return customers
 
 
 def change_customer_data(id, name, email, subscription):
     is_update_sucessfull = False
-    costumers = data_manager.read_table_from_file('crm.csv', ';')
+    costumers = data_manager.read_table_from_file(DATAFILE, ';')
     for costumer_index in range(len(costumers)):
         if id == costumers[costumer_index][0]:
             costumers[costumer_index][1] = name
             costumers[costumer_index][2] = email
             costumers[costumer_index][3] = subscription
             is_update_sucessfull = True
-    data_manager.write_table_to_file('crm.csv', costumers, ';')
+    data_manager.write_table_to_file(DATAFILE, costumers, ';')
     return is_update_sucessfull
 
 
 def remove_customer_data(id):
     is_costumer = False
-    costumers = data_manager.read_table_from_file('crm.csv', ';')
+    costumers = data_manager.read_table_from_file(DATAFILE, ';')
     for costumer_index in range(len(costumers)):
         if id == costumers[costumer_index][0]:
             costumers.remove(costumers[costumer_index])
             is_costumer = True
-    data_manager.write_table_to_file('crm.csv', costumers, ';')
+    data_manager.write_table_to_file(DATAFILE, costumers, ';')
     return is_costumer
 
 
 def subscribed_customers():
     subscribed_costumer = []
-    costumers = data_manager.read_table_from_file('crm.csv', ';')
+    costumers = data_manager.read_table_from_file(DATAFILE, ';')
     for costumer_index in range(len(costumers)):
         if 1 == costumers[costumer_index][3]:
             subscribed_costumer.append(costumers[costumer_index][2])
     return subscribed_costumer
-
-
-DATAFILE = "model/crm/crm.csv"
-HEADERS = ["id", "name", "email", "subscribed"]
