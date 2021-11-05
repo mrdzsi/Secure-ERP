fbff3b4f95a7257fa8a21f032cb0c12f7bf1f0a5
horvathsanyi <horvathsanyi0202@gmail.com>
2021-11-05 10:04:48 +0100

    now working print_general_results()

--- view/terminal.py
+++ view/terminal.py
@@ -35,12 +35,11 @@ def print_general_results(result, label):
     """
     # if type(result) == int or float:
     #     print(f"{label}: {round(result, 2)}")
-    if type(result) == dict:
+    if type(result) == list or type(result) == tuple:
+        print(f"{label}:\n{'; '.join(result)}")
+    elif type(result) == dict:
         result = "; ".join(f"{key}: {value}" for key, value in result.items())
         print(f"{label}:\n{result}")
-    elif type(result) == list or tuple:
-        print(f"{label}:\n{'; '.join(result)}")
-    
 
 
 # /--------------------------------\
